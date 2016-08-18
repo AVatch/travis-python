325a26c6ba7cc8131ac18873ee31c0c8913961f1
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-08-18 11:28:38 -0400

    yolo

--- code.py
+++ code.py
@@ -7,5 +7,10 @@ This module demonstrates documentation as
 def main():
     print 12
 
+def yolo():
+    """The yolo function yolos
+    """
+    print "yolo"
+
 if __name__ == "__main__":
     main()
